ef8ea3c2e2a60337a59cda29802723e90b2e4588
hakutakaid <hakutakaid@users.noreply.github.com>
2025-10-27 13:43:08 +0700

    first commit

---
+++ neki.sh
@@ -0,0 +1,7 @@
+echo "# MEKI" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rahmaoktaviani172-alt/MEKI.git
+git push -u origin main
